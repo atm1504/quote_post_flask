684ba37ff8eaf48f35bee67d601e88f98f5f6f61
Amartya Mondal <hayyoulistentome@gmail.com>
2020-06-01 21:06:09 +0530

    feat: App template and basic code

--- flaskBlog.py
+++ flaskBlog.py
@@ -1,13 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+
+# Dummy data
+posts = [
+    {
+        'author': 'Atreyee Mukherjee',
+        'title': "B'Day Wish",
+        'content': "Happy B'day Amartya Darling! ",
+        'date_posted': '4th Jan, 2000'
+    },
+    {
+        'author': 'Amartya Mondal',
+        'title': "B'Day Wish",
+        'content': "Happy B'day Atreyee Darling! ",
+        'date_posted': '15th August, 2000'
+    }
+]
 
 @app.route('/')
 def hello():
-    return '<h1>Hello, World!</h1>'
+    return render_template("home.html", posts=posts, title="ATM")
 
 @app.route('/about')
 def about():
-    return '<h1>Hello, about page here</h1>'
+    return render_template("about.html", posts=posts)
 
 if __name__ == "__main__":
     app.run(debug=True)
