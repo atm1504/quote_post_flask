eecd484d6a4193e5459e209afe1a6ab6f313bd0e
Amartya Mondal <atm1504.in@gmail.com>
2020-06-06 01:34:22 +0530

    feat: Add new post ui

--- quote_post/routes.py
+++ quote_post/routes.py
@@ -1,7 +1,7 @@
 from flask import render_template, url_for, flash, redirect, request
 from quote_post import app, db, bcrypt
 from PIL import Image
-from quote_post.forms import RegistrationForm, LoginForm, UpdateAccountForm
+from quote_post.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from quote_post.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 import secrets
@@ -109,3 +109,13 @@ def account():
         form.email.data = current_user.email
     image_file = url_for('static', filename="profile_pics/" + current_user.image_file)
     return render_template("account.html", title="Account", image_file=image_file, form=form)
+
+@app.route("/post/new",methods=["GET", "POST"])
+@login_required
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        flash("Your post has been created!", "success")
+        return redirect(url_for("home"))
+    return render_template("create_post.html", title="New Post", form=form)
+    
