d9326f037702fabc496e9e1240cea7f0ae06a268
Amartya Mondal <hayyoulistentome@gmail.com>
2020-06-06 00:31:27 +0530

    feat: Update profile details

--- quote_post/routes.py
+++ quote_post/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, url_for, flash, redirect, request
 from quote_post import app,db,bcrypt
-from quote_post.forms import RegistrationForm, LoginForm
+from quote_post.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from quote_post.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 # Dummy data
@@ -75,8 +75,18 @@ def logout():
     logout_user()
     return redirect(url_for("home"))
 
-@app.route("/account")
+@app.route("/account", methods=["GET", "POST"])
 @login_required
 def account():
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash("Your account details has been updated", "success")
+        return redirect(url_for("account"))
+    elif request.method == "GET":
+        form.username.data = current_user.username
+        form.email.data = current_user.email
     image_file = url_for('static', filename="profile_pics/" + current_user.image_file)
-    return render_template("account.html", title="Account", image_file=image_file)
+    return render_template("account.html", title="Account", image_file=image_file, form=form)
