c9cfdd3c42a049e3d3000f396742348d4e59a3d9
Amartya Mondal <atm1504.in@gmail.com>
2020-06-06 18:56:51 +0530

    feat: Add pagination

--- quote_post/routes.py
+++ quote_post/routes.py
@@ -8,10 +8,12 @@ import secrets
 import os
 
 @app.route('/')
+@app.route("/home")
 def home():
     if current_user.is_authenticated==False:
             return redirect(url_for("login"))
-    posts= Post.query.all()
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.paginate(per_page=5, page=page)
     return render_template("home.html", posts=posts, title="ATM")
 
 @app.route('/about')
